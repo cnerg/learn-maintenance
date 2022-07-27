b1594579632b6c3569577c9005ded3f2dcc20b55
bquan0 <powerbowen@gmail.com>
2022-07-27 12:59:12 -0500

    length conversion program

--- prog.py
+++ prog.py
@@ -1,22 +1,54 @@
 import argparse
 from itertools import count
 from random import choices
-import random
 
-parser = argparse.ArgumentParser()
-parser.add_argument("input", type = int, help = "guess for a random 1 digit number", choices = range(10))
-parser.add_argument("-v", "--verbosity", help = "increase output verbosity", action = "count", default = 0)
-args = parser.parse_args()
-num = random.randrange(0,10)
+def unitToInch(unit, amt):
+    if unit == "in":
+        return amt
+    elif unit == "ft":
+        return amt * 12
+    elif unit == "yd":
+        return amt * 36
+    elif unit == "mi":
+        return amt * 15840
+    elif unit == "cm":
+        return amt/2.54
+    elif unit == "m":
+        return amt/2.54 * 100
+    elif unit == "km":
+        return amt/2.54 * 100000
 
-if args.input == num:
-    outcome = "right"
-else:
-    outcome = "wrong"
-if args.verbosity >= 2:
-    print("Your guess was " + outcome + ". The correct number was " + str(num))
-elif args.verbosity >= 1:
-    print("Your guess was " + outcome + ".")
-else:
-    print(outcome)
+def inchToUnit(unit, amt):
+    if unit == "in":
+        return amt
+    elif unit == "ft":
+        return amt/12
+    elif unit == "yd":
+        return amt/36
+    elif unit == "mi":
+        return amt/15840
+    elif unit == "cm":
+        return amt * 2.54
+    elif unit == "m":
+        return amt * 2.54 / 100
+    elif unit == "km":
+        return amt * 2.54 / 100000
+
+# TODO add argparser here
+def main():
+    parser = argparse.ArgumentParser(prog = "number guesser")
+    parser.add_argument("input_amount", type = float, help = "input amount")
+    parser.add_argument("input_unit", choices = ["in", "ft", "yd", "mi", "cm", "m", "km"], help = "unit of the input amount")
+    parser.add_argument("output_unit", choices = ["in", "ft", "yd", "mi", "cm", "m", "km"], help = "unit you want to convert to")
+    parser.add_argument("-v", "--verbosity", help = "increase output verbosity", action = "count", default = 0)
+    args = parser.parse_args()
+
+    output_amount = inchToUnit(args.output_unit, unitToInch(args.input_unit, args.input_amount))
+    if args.verbosity >= 1:
+        print(str(args.input_amount) + args.input_unit + " is " + str(output_amount) + args.output_unit)
+    else:
+        print(str(output_amount) + args.output_unit)
+
+if __name__ == '__main__':
+    main()
     
